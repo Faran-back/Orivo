1f7fbb2b6f13b63db8af1ae8c15e8a4e93fef22a
faran-back <inotfarhan@gmail.com>
2025-09-05 18:02:53 +0500

    changes

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+// import vue from '@inertiajs/vue3';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+        vue(),
+    ],
+});
