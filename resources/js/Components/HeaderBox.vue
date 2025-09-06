e1c70134e597a7eaf36328f8366bfad269356409
faran-back <inotfarhan@gmail.com>
2025-09-06 17:35:58 +0500

    Make a component for HeaderBox and use it in Root Index

---
+++ resources/js/Components/HeaderBox.vue
@@ -0,0 +1,27 @@
+<script setup>
+    defineProps({
+      type: String,
+      title: String,
+      user: String,
+      subtext: String
+    })
+</script>
+
+<template>
+    <div class="header-box">
+        <h1 
+            class="header-box-title">
+            {{ title }}
+            <span 
+                v-if="type === 'greeting'"
+                class="text-bankGradient"> 
+             {{ user }}
+            </span>
+        </h1>
+        <p
+            class="header-box-subtext">
+            {{ subtext }}
+        </p>
+    </div>
+</template>
+
--- resources/js/Pages/Root/Index.vue
+++ resources/js/Pages/Root/Index.vue
@@ -1,9 +1,21 @@
 <template>
   <RootLayout>
-    <h1>Home</h1>
+    <section class="home">
+        <div class="home-content">
+            <header class="home-header">
+                <HeaderBox
+                    type="greeting"
+                    title="Welcome"
+                    user="Faran"
+                    subtext="Access and manage your accounts and transactions"
+                />
+            </header>
+        </div>
+    </section>
   </RootLayout>
 </template>
 
 <script setup>
-import RootLayout from '@/Layouts/RootLayout.vue'
+    import RootLayout from '@/Layouts/RootLayout.vue'
+    import HeaderBox from '@/Components/HeaderBox.vue'
 </script>
